832586d14580313be3eea0ed252546c6669b3c0a
lexejj <lexejj@gmail.com>
2020-11-09 21:06:56 +0300

    Added icons

--- Lantern/ViewController.swift
+++ Lantern/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
+var screenColor = 0
+
 class ViewController: UIViewController {
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    fileprivate func updateUi() {
+        if screenColor == 0 {
+       view.backgroundColor = .white
+        } else if screenColor == 1 {
+            view.backgroundColor = .blue
+        } else if screenColor == 2 {
+            view.backgroundColor = .red
+        } else if screenColor == 3 {
+            view.backgroundColor = .cyan
+        } else if screenColor == 4 {
+            view.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUi()
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        screenColor += 1
+        updateUi()
+        if screenColor >= 4 {
+       screenColor = -1
+    }
+ 
 }
-
+}
