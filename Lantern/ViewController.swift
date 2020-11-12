e88d68155dff0deb736a165d6764d1091fcafaa2
lexejj <lexejj@gmail.com>
2020-11-12 19:19:06 +0300

    Отформатирован код, добавлены комментарии, добавлена функция включения фонарика на телефоне.

--- Lantern/ViewController.swift
+++ Lantern/ViewController.swift
@@ -6,17 +6,24 @@
 //
 
 import UIKit
-
-var screenColor = 0
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var screenColor = 0
+    var isLightOn = true
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+    /// Функция определяет цвет экрана в зависимости от значения переменной.
     fileprivate func updateUi() {
         if screenColor == 0 {
-       view.backgroundColor = .white
+            view.backgroundColor = .white
         } else if screenColor == 1 {
             view.backgroundColor = .blue
         } else if screenColor == 2 {
@@ -27,17 +34,28 @@ class ViewController: UIViewController {
             view.backgroundColor = .black
         }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateUi()
+    /// Функция включает физичиский фонарик на телефоне после касания
+    func updateView() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = isLightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         screenColor += 1
         updateUi()
+        updateView()
         if screenColor >= 4 {
-       screenColor = -1
+            screenColor = -1
+        }
+        
     }
- 
-}
 }
